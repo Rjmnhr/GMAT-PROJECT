c08a7a7eeb9e5ba2f582cadee1e0511a93685f5e
rjmnhr1 <renjith.cm@refactor.academy>
2023-09-25 21:33:05 +0530

    update

--- src/pages/otp-verification/index.js
+++ src/pages/otp-verification/index.js
@@ -126,7 +126,7 @@ const OtpVerification = () => {
             className="left-container img_container"
             style={{
               backgroundImage:
-                "url(https://lh5.googleusercontent.com/5fVJIifi1CIJ76Og-mHkXlY2Us2xcnaboBFakE2UVDbfMANy4tv_FFs4gZyMkiK6FduI2GhCXiu79qwMbkyMpu8=w16383)",
+                "url(https://res.cloudinary.com/dsw1ubwyh/image/upload/v1695657433/pgsgfjhvitdmjl0pnbco.png)",
               backgroundPosition: "center",
               backgroundSize: "cover",
               height: "100vh",
