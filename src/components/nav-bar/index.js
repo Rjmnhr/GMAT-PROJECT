ef667e6463d202cf98357064d0aba56830b91069
rjmnhr1 <renjith.cm@refactor.academy>
2023-09-27 21:31:30 +0530

    update

--- src/components/nav-bar/index.js
+++ src/components/nav-bar/index.js
@@ -8,14 +8,28 @@ const NavBar = () => {
   const user_name = localStorage.getItem("user_name");
 
   const handleLogOut = () => {
-    navigate("/");
+    navigate("https://www.adefteducation.com");
     localStorage.setItem("accessToken", "");
     localStorage.setItem("isLoggedIn", false);
   };
   return (
-    <div className="container-fluid p-3 d-flex justify-content-between align-items-center border-bottom">
+    <div className="container-fluid  d-flex justify-content-between align-items-center border-bottom">
       <div className="d-flex justify-content-between align-items-center">
-        <h3 onClick={() => navigate("/")}>ADEFT EDUCATION</h3>
+        <div className="d-flex align-items-center ">
+          <img
+            src="https://res.cloudinary.com/dsw1ubwyh/image/upload/v1695818576/ichnuzqxvbetqcymqti4.svg"
+            alt=""
+            width={60}
+            height={60}
+          />
+          <p
+            className="fs-3 pb-1"
+            onClick={() => navigate("https://www.adefteducation.com")}
+          >
+            ADEFT EDUCATION
+          </p>
+        </div>
+
         {/* <button className="btn border">Dashboard</button> */}
       </div>
 
