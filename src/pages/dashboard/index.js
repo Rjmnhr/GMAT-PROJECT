c738dd7d2794383b6dbfbf028659802f54c1fb5e
Rjmnhr <88718714+Rjmnhr@users.noreply.github.com>
2023-11-23 18:12:26 +0530

    Update index.js

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -12,7 +12,13 @@ const DashBoardComponent = () => {
   const [practiceExam3, setPracticeExam3] = useState(null);
 
   const user_id = localStorage.getItem("user_id");
+  useEffect(() => {
+    const isLoggedIn = localStorage.getItem("isLoggedIn");
 
+    if (isLoggedIn !== "true") {
+      navigate("/login-app");
+    }
+  });
   const fetchData = async (examNo) => {
     try {
       const response = await AxiosInstance.post("/api/exams/get-data", {
