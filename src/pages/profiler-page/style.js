657dc328d659cf0e49c808c11a4f63c5ed757275
rjmnhr1 <renjith.cm@refactor.academy>
2023-12-05 02:15:59 +0530

    update

--- src/pages/profiler-page/style.js
+++ src/pages/profiler-page/style.js
@@ -32,23 +32,6 @@ export const ProfilerPageStyled = styled.div`
     margin-left: 15px;
   }
 
-  .css-1h9z7r5-MuiButtonBase-root-MuiTab-root {
-    margin-bottom: 20px;
-  }
-  .css-1h9z7r5-MuiButtonBase-root-MuiTab-root {
-    border-right: 5px solid #cfcecc;
-    border-bottom: none;
-  }
-  .css-10d9dml-MuiTabs-indicator {
-    position: absolute;
-    height: 100%;
-    bottom: 0;
-    width: 5px;
-    -webkit-transition: all 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
-    transition: all 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
-    background-color: rgb(0, 128, 128);
-    right: 0;
-  }
   @media (max-width: 912px) {
     .background-container {
       background: none;
