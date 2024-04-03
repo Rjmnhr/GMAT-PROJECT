bb72f65140f713d2bea1389f6889a5ef68886e94
rjmnhr1 <renjith.cm@refactor.academy>
2024-04-04 00:42:02 +0530

    update

--- src/Config/axios.js
+++ src/Config/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const AxiosInstance = axios.create({
-  // baseURL: "https://adeftbackend1-7xwgos42.b4a.run",
-  baseURL: "http://localhost:8003",
+  baseURL: "https://adeftbackend1-7xwgos42.b4a.run",
+  // baseURL: "http://localhost:8003",
 });
 
 export default AxiosInstance;
